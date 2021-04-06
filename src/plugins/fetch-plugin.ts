b0908aad1b68fc5861755e0330c3746cc550bc4d
Leftyxiv <manny@aledoux.net>
2021-04-06 14:34:18 -0400

    successfully bundles css

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -25,9 +25,23 @@ export const fetchPlugin = (inputCode: string) => {
         }
         // if not cached
         const { data, request } = await axios.get(args.path);
+
+        const fileType = args.path.match(/.css$/) ? "css" : "jsx";
+
+        const escaped = data.replace(/\n/g, "").replace(/"/g, '\\"').replace(/'/g, "\\'");
+
+        const contents =
+          fileType === "css"
+            ? `
+        const style = document.createElement('style');
+        style.innerText = '${escaped}';
+        document.head.appendChild(style)
+        `
+            : data;
+
         const result: esbuild.OnLoadResult = {
           loader: "jsx",
-          contents: data,
+          contents,
           resolveDir: new URL("./", request.responseURL).pathname,
         };
         // store in cache
