036b7d2f917253760531390a438eda08e5a099a9
Leftyxiv <manny@aledoux.net>
2021-04-06 12:38:50 -0400

    correctly gets nested files from require statements from unkpg

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -9,9 +9,22 @@ export const unpkgPathPlugin = () => {
         console.log("onResolve", args);
         if (args.path === "index.js") {
           return { path: args.path, namespace: "a" };
-        } else if (args.path === "tiny-test-pkg") {
-          return { path: "https://unpkg.com/tiny-test-pkg@1.0.0/index.js", namespace: "a" };
         }
+
+        if (args.path.includes("./") || args.path.includes("../")) {
+          return {
+            namespace: "a",
+            path: new URL(args.path, `https://unpkg.com${args.resolveDir}/`).href,
+          };
+        }
+
+        return {
+          namespace: "a",
+          path: `https://unpkg.com/${args.path}`,
+        };
+        // else if (args.path === "tiny-test-pkg") {
+        //   return { path: "https://unpkg.com/tiny-test-pkg@1.0.0/index.js", namespace: "a" };
+        // }
       });
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
@@ -21,15 +34,16 @@ export const unpkgPathPlugin = () => {
           return {
             loader: "jsx",
             contents: `
-              const message = require('tiny-test-pkg');
+              const message = require('nested-test-pkg');
               console.log(message);
             `,
           };
         }
-        const { data } = await axios.get(args.path);
+        const { data, request } = await axios.get(args.path);
         return {
           loader: "jsx",
           contents: data,
+          resolveDir: new URL("./", request.responseURL).pathname,
         };
       });
     },
