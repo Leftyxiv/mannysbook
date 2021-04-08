c6c1b18af247ddfb380e0c942393dc46f89192ae
Leftyxiv <manny@aledoux.net>
2021-04-08 19:00:57 -0400

    got the iframe in, need to change the context still

--- src/index.tsx
+++ src/index.tsx
@@ -41,7 +41,15 @@ const App = () => {
       },
     });
     setCode(result.outputFiles[0].text);
+
+
   };
+
+  const html = `
+  <script>
+  ${code}
+  </script>
+  `
   return (
     <div>
       <textarea value={input} onChange={(e) => setInput(e.target.value)}></textarea>
@@ -49,6 +57,7 @@ const App = () => {
         <button onClick={onClick}>Submit</button>
       </div>
       <pre>{code}</pre>
+      <iframe sandbox="allow-scripts" srcDoc={html} />
     </div>
   );
 };
