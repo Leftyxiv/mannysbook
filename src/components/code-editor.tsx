6621dd1702eef0bf2ec1cdd505cefece3f10716f
Leftyxiv <manny@aledoux.net>
2021-04-09 09:36:35 -0400

    added word wrap config

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,7 +1,10 @@
 import MonacoEditor from "@monaco-editor/react";
 
 const CodeEditor = () => {
-  return <MonacoEditor language="javascript" height="300px" />;
+  return <MonacoEditor theme='dark' language="javascript" height="300px" options={{
+    wordWrap: 'on',
+    minimap: ,
+  }}/>;
 };
 
 export default CodeEditor;
