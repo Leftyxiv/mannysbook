ea06e49761f2ea88b90fe7070e8211b3e28a696d
Leftyxiv <manny@aledoux.net>
2021-04-09 08:29:30 -0400

    sandbox is in and working, woo!

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,7 @@ import { fetchPlugin } from "./plugins/fetch-plugin";
 
 const App = () => {
   const ref = useRef<any>();
+  const iframe = useRef<any>();
   const [input, setInput] = useState("");
   const [code, setCode] = useState("");
 
@@ -40,16 +41,23 @@ const App = () => {
         global: "window",
       },
     });
-    setCode(result.outputFiles[0].text);
-
-
+    //setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, "*");
   };
 
   const html = `
-  <script>
-  ${code}
-  </script>
-  `
+  <html>
+    <head></head>
+    <body>
+      <div id='root'></div>
+      <script>
+        window.addEventListener('message', (event) => {
+          eval(event.data);
+        }, false)
+      </script>
+    </body>
+  </html>
+  `;
   return (
     <div>
       <textarea value={input} onChange={(e) => setInput(e.target.value)}></textarea>
@@ -57,7 +65,7 @@ const App = () => {
         <button onClick={onClick}>Submit</button>
       </div>
       <pre>{code}</pre>
-      <iframe sandbox="allow-scripts" srcDoc={html} />
+      <iframe ref={iframe} sandbox="allow-scripts" srcDoc={html} />
     </div>
   );
 };
