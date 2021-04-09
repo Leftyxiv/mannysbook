de0989936a700436839a86f897a94a48dc5ecaf9
Leftyxiv <manny@aledoux.net>
2021-04-09 09:40:54 -0400

    completed editor configuration

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,10 +1,23 @@
 import MonacoEditor from "@monaco-editor/react";
 
 const CodeEditor = () => {
-  return <MonacoEditor theme='dark' language="javascript" height="300px" options={{
-    wordWrap: 'on',
-    minimap: ,
-  }}/>;
+  return (
+    <MonacoEditor
+      theme="dark"
+      language="javascript"
+      height="300px"
+      options={{
+        wordWrap: "on",
+        minimap: { enabled: false },
+        showUnused: false,
+        folding: false,
+        lineNumbersMinChars: 3,
+        fontSize: 16,
+        scrollBeyondLastLine: false,
+        automaticLayout: true
+      }}
+    />
+  );
 };
 
 export default CodeEditor;
