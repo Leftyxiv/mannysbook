fae123f6e48f14feae155e43f47577bd895e58a3
Leftyxiv <manny@aledoux.net>
2021-04-11 12:21:17 -0400

    refactored everything into it's own component

---
+++ src/components/code-cell.tsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+import Preview from "./preview";
+import CodeEditor from "./code-editor";
+import bundle from "../bundler";
+
+const CodeCell = () => {
+  const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
+
+  const onClick = async () => {
+    const output = await bundle(input);
+    setCode(output);
+  };
+
+  return (
+    <div>
+      <CodeEditor onChange={(value) => setInput(value)} initialValue="const a = 1;" />
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <Preview code={code} />
+    </div>
+  );
+};
+
+export default CodeCell;
--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,15 @@
 import "bulmaswatch/superhero/bulmaswatch.min.css";
 
-import { useState } from "react";
 import ReactDOM from "react-dom";
 
-import Preview from "./components/preview";
-import CodeEditor from "./components/code-editor";
-import bundle from "./bundler";
+import CodeCell from './components/code-cell';
 
 const App = () => {
-  const [input, setInput] = useState("");
-  const [code, setCode] = useState("");
 
-  const onClick = async () => {
-    // if the service hasn't been run yet just exit
-    const output = await bundle(input);
-    setCode(output);
-  };
 
   return (
     <div>
-      <CodeEditor onChange={(value) => setInput(value)} initialValue="const a = 1;" />
-      <div>
-        <button onClick={onClick}>Submit</button>
-      </div>
-      <Preview code={code} />
+      <CodeCell />
     </div>
   );
 };
