656f7fe96d3b8a79062e00eab8ce876d7a15016e
Leftyxiv <manny@aledoux.net>
2021-04-11 11:39:48 -0400

    added jsx syntax highlighting and some styling

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,8 +1,11 @@
-import './code-editor.css'
+import "./code-editor.css";
+import './syntax.css'
 import { useRef } from "react";
 import MonacoEditor, { EditorDidMount } from "@monaco-editor/react";
 import prettier from "prettier";
 import parser from "prettier/parser-babel";
+import codeShift from "jscodeshift";
+import Highlighter from "monaco-jsx-highlighter";
 
 interface CodeEditorProps {
   initialValue: string;
@@ -19,22 +22,38 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, onChange }) => {
     });
     // tab spacing from 4 to 2
     monacoEditor.getModel()?.updateOptions({ tabSize: 2 });
+
+    const highlighter = new Highlighter(
+      // @ts-ignore
+      window.monaco,
+      codeShift,
+      monacoEditor,
+    );
+    highlighter.highLightOnDidChangeModelContent(
+      ()=>{},
+      ()=>{},
+      undefined,
+      ()=>{},
+    );
   };
 
   const onFormatClick = () => {
     const unformatted = editorRef.current.getModel().getValue();
-    const formatted = prettier.format(unformatted, {
-      parser: "babel",
-      plugins: [parser],
-      useTabs: false,
-      semi: true,
-      singleQuote: true,
-    });
+    const formatted = prettier
+      .format(unformatted, {
+        parser: "babel",
+        plugins: [parser],
+        useTabs: false,
+        semi: true,
+        singleQuote: true,
+        // replaces \n from prettier with empty string
+      })
+      .replace(/\n$/, "");
 
     editorRef.current.setValue(formatted);
   };
   return (
-    <div className='editor-wrapper'>
+    <div className="editor-wrapper">
       <button className="button button-format is-primary is-small" onClick={onFormatClick}>
         Format
       </button>
