2e15187d4ac717ab3c38e6c5871919d1e97fa589
Leftyxiv <manny@aledoux.net>
2021-04-11 17:46:46 -0400

    added timeout for message to post to iframe

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -35,7 +35,9 @@ const Preview: React.FC<PreviewProps> = ({ code }) => {
   const iframe = useRef<any>();
   useEffect(() => {
     iframe.current.srcdoc = html;
-    iframe.current.contentWindow.postMessage(code, "*");
+    setTimeout(() => {
+      iframe.current.contentWindow.postMessage(code, "*");
+    }, 50);
   }, [code]);
   return (
     <div className="preview-wrapper">
