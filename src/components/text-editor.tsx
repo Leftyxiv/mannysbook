f8d6a36ce9af68c28919adb674339af56c43510c
Leftyxiv <manny@aledoux.net>
2021-04-12 09:26:00 -0400

    markdown editor now opens and closes upon the correct clicks

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -1,11 +1,15 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import MDEditor from "@uiw/react-md-editor";
 
 const TextEditor: React.FC = () => {
   const [editing, setEditing] = useState(false);
+  const ref = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
-    const listener = () => {
+    const listener = (event: MouseEvent) => {
+      if(ref.current && event.target && ref.current.contains(event.target as Node)){
+        return;
+      }
       setEditing(false)
     }
     document.addEventListener('click', listener, { capture: true })
@@ -13,11 +17,11 @@ const TextEditor: React.FC = () => {
       document.removeEventListener('click', listener, { capture: true })
     }
   }, [])
-  
+
 
   if (editing) {
     return (
-      <div>
+      <div ref={ref}>
         <MDEditor />
       </div>
     );
