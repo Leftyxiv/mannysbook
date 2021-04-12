f1f1b05d7d8ef555146a4d51b651ffd5e8c6ba0a
Leftyxiv <manny@aledoux.net>
2021-04-12 15:41:18 -0400

    refactored local input state from codecell into global store

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -5,19 +5,20 @@ import CodeEditor from "./code-editor";
 import bundle from "../bundler";
 import Resizable from "./resizable";
 import { Cell } from '../state';
+import { useActions } from '../hooks/use-actions';
 
 interface CodeCellProps {
   cell: Cell;
 }
 
 const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
-  const [input, setInput] = useState("");
   const [code, setCode] = useState("");
   const [err, setErr] = useState("");
+  const { updateCell } = useActions();
 
   useEffect(() => {
     const timer = setTimeout(async () => {
-      const output = await bundle(input);
+      const output = await bundle(cell.content);
       setCode(output.code);
       setErr(output.err);
     }, 1000);
@@ -25,13 +26,13 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
     return () => {
       clearTimeout(timer)
     }
-  }, [input]);
+  }, [cell.content]);
 
   return (
     <Resizable direction="vertical">
       <div style={{ height: "100%", display: "flex", flexDirection: "row" }}>
         <Resizable direction="horizontal">
-          <CodeEditor onChange={(value) => setInput(value)} initialValue="const a = 1;" />
+          <CodeEditor onChange={(value) => updateCell(cell.id, value)} initialValue={cell.content} />
         </Resizable>
         <Preview code={code} err={err}/>
       </div>
