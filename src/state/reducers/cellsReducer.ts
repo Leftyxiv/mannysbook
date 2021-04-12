924b9c2245089e02b858c0209f0fb62be5c90aba
Leftyxiv <manny@aledoux.net>
2021-04-12 13:40:38 -0400

    added functionality for the move cell reducer

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -30,6 +30,14 @@ const reducer = produce((state: CellsState = initialState, action: Action) => {
       state.order = state.order.filter((id) => id !== action.payload);
       return;
     case ActionType.MOVE_CELL:
+      const { direction } = action.payload;
+      const index = state.order.findIndex((id)=> id === action.payload.id )
+      const targetIndex = direction === 'up' ? index - 1 : index + 1;
+      if (targetIndex < 0 || targetIndex > state.order.length - 1){
+        return ;
+      }
+      state.order[index] = state.order[targetIndex];
+      state.order[targetIndex] = action.payload.id
       return state;
     case ActionType.INSERT_CELL_BEFORE:
       return state;
