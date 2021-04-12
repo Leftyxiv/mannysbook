38c423a359eb50ab804baf7f56cdf89be71de4bc
Leftyxiv <manny@aledoux.net>
2021-04-12 08:03:58 -0400

    added a markdown editor

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,14 @@ import "bulmaswatch/superhero/bulmaswatch.min.css";
 
 import ReactDOM from "react-dom";
 
-import CodeCell from './components/code-cell';
+// import CodeCell from './components/code-cell';
+import TextEditor from "./components/text-editor";
 
 const App = () => {
-
   return (
     <div>
-      <CodeCell />
+      {/* <CodeCell /> */}
+      <TextEditor />
     </div>
   );
 };
