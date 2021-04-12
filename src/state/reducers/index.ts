1991042a564584ce193c3d7ce14810c16551af86
Leftyxiv <manny@aledoux.net>
2021-04-12 10:36:58 -0400

    added store

--- src/state/reducers/index.ts
+++ src/state/reducers/index.ts
@@ -1,1 +1,10 @@
-export {}
+import { combineReducers } from 'redux'
+import cellsReducer from './cellsReducer'
+
+const reducers = combineReducers({
+  cells: cellsReducer
+});
+
+export default reducers;
+
+export type RootState = ReturnType<typeof reducers>;
