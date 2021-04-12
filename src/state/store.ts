826c7dfa13d294b8f4f8b9b1c10dbda4c9c5f070
Leftyxiv <manny@aledoux.net>
2021-04-12 15:23:27 -0400

    added useAction hook

--- src/state/store.ts
+++ src/state/store.ts
@@ -14,5 +14,21 @@ store.dispatch({
   type: ActionType.INSERT_CELL_BEFORE,
   payload: { id: null, type: "text" },
 });
+store.dispatch({
+  type: ActionType.INSERT_CELL_BEFORE,
+  payload: { id: null, type: "code" },
+});
+store.dispatch({
+  type: ActionType.INSERT_CELL_BEFORE,
+  payload: { id: null, type: "text" },
+});
+store.dispatch({
+  type: ActionType.INSERT_CELL_BEFORE,
+  payload: { id: null, type: "code" },
+});
+store.dispatch({
+  type: ActionType.INSERT_CELL_BEFORE,
+  payload: { id: null, type: "text" },
+});
 
 console.log(store.getState());
