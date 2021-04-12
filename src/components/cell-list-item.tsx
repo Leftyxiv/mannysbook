6e86e87498571c91d2787324459481e3fbc75c43
Leftyxiv <manny@aledoux.net>
2021-04-12 15:16:55 -0400

    can properly render either editor to the screen

--- src/components/cell-list-item.tsx
+++ src/components/cell-list-item.tsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import { Cell } from '../state';
 
+import CodeCell from './code-cell';
+import TextEditor from './text-editor';
 interface CellListItemProps {
   cell: Cell;
 }
 
 const CellListItem: React.FC<CellListItemProps> = ({ cell }) => {
+  let child: JSX.Element;
+  if(cell.type === 'code'){
+    child = <CodeCell />
+  } else {
+    child = <TextEditor />
+  }
   return (
     <div>
-      { cell.id }
+      { child }
     </div>
   )
 }
