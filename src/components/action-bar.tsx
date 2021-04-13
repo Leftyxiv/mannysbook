0394fb4d6418afa211d6e7129a4c95fb07e3469e
Leftyxiv <manny@aledoux.net>
2021-04-13 09:49:17 -0400

    refactored the buttons into their own component

--- src/components/action-bar.tsx
+++ src/components/action-bar.tsx
@@ -1,18 +1,34 @@
-import React from 'react'
-import { useActions } from '../hooks/use-actions'
+import React from "react";
+import { useActions } from "../hooks/use-actions";
 
+import ActionButton from "./action-button";
 interface ActionBarProps {
   id: string;
 }
-const ActionBar:React.FC<ActionBarProps> = ({ id }) => {
+const ActionBar: React.FC<ActionBarProps> = ({ id }) => {
   const { moveCell, deleteCell } = useActions();
   return (
-    <div>
-      <button onClick={()=> moveCell(id, 'up')}>Up</button>
-      <button onClick={()=> moveCell(id, 'down')}>Down</button>
-      <button onClick={() => deleteCell(id)}>Delete</button>
+    <div style={{ display: 'flex' }}>
+      <ActionButton onClick={moveCell} direction="up" icon="fa-arrow-up" id={id} />
+      <ActionButton onClick={moveCell} direction="down" icon="fa-arrow-down" id={id} />
+      <ActionButton onClick={deleteCell} icon="fa-times" id={id} />
+      {/* <button className="button is-primary is-small" onClick={() => moveCell(id, "up")}>
+        <span className="icon">
+          <i className="fas fa-arrow-up" />
+        </span>
+      </button>
+      <button className="button is-primary is-small" onClick={() => moveCell(id, "down")}>
+        <span className="icon">
+          <i className="fas fa-arrow-down" />
+        </span>
+      </button>
+      <button className="button is-primary is-small" onClick={() => deleteCell(id)}>
+        <span className="icon">
+          <i className="fas fa-times" />
+        </span>
+      </button> */}
     </div>
-  )
-}
+  );
+};
 
 export default ActionBar;
---
+++ src/components/action-button.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+interface ActionButtonProps {
+  icon: string;
+  id: string;
+  onClick(id: string, direction?: string): void;
+  direction?: string;
+}
+
+const ActionButton: React.FC<ActionButtonProps> = ({icon, id, onClick, direction}) => {
+  return (
+    <div>
+        <button className="button is-primary is-small" onClick={() => onClick(id, direction)}>
+        <span className="icon">
+          <i className={`fas ${icon}`} />
+        </span>
+      </button>
+    </div>
+  )
+}
+
+export default ActionButton;
