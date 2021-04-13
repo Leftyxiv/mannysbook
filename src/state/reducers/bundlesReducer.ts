0d37733b2669be3cfcf5f8636280db637a206e79
Leftyxiv <manny@aledoux.net>
2021-04-13 15:10:13 -0400

    got the scaffolding for bundle reducer in

--- src/state/reducers/bundlesReducer.ts
+++ src/state/reducers/bundlesReducer.ts
@@ -1,1 +1,27 @@
-export {}
+import produce from 'immer';
+
+import { Action } from './../actions/index';
+import { ActionType } from './../action-types/index';
+
+interface BundlesState {
+  [key: string]: {
+    loading: boolean;
+    code: string;
+    err: string;
+  }
+}
+
+const initialState: BundlesState = {}
+
+const reducer = produce((state: BundlesState = initialState, action: Action):BundlesState => {
+  switch(action.type){
+    case ActionType.BUNDLE_START:
+      return state;
+    case ActionType.BUNDLE_COMPLETE:
+      return state;
+    default:
+      return state
+  }
+})
+
+export default reducer;
