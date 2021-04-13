e543c5b05b50033448c384be9fd631ff02640a87
Leftyxiv <manny@aledoux.net>
2021-04-13 13:26:33 -0400

    added some styling

--- src/components/cell-list-item.tsx
+++ src/components/cell-list-item.tsx
@@ -1,27 +1,32 @@
-import './cell-list-item.css'
-import React from 'react'
-import { Cell } from '../state';
+import "./cell-list-item.css";
+import React from "react";
+import { Cell } from "../state";
 
-import CodeCell from './code-cell';
-import TextEditor from './text-editor';
-import ActionBar from './action-bar';
+import CodeCell from "./code-cell";
+import TextEditor from "./text-editor";
+import ActionBar from "./action-bar";
 interface CellListItemProps {
   cell: Cell;
 }
 
 const CellListItem: React.FC<CellListItemProps> = ({ cell }) => {
   let child: JSX.Element;
-  if(cell.type === 'code'){
-    child = <CodeCell cell={cell} />
+  if (cell.type === "code") {
+    child = (
+      <>
+        <div className="action-bar-wrapper">
+          <ActionBar id={cell.id} />
+        </div>
+        <CodeCell cell={cell} />
+      </>
+    );
   } else {
-    child = <TextEditor cell={cell} />
+    child = <>
+    <ActionBar id={cell.id}/>
+    <TextEditor cell={cell} />
+    </>
   }
-  return (
-    <div className='cell-list-item'>
-      <ActionBar id={cell.id}/>
-      { child }
-    </div>
-  )
-}
+  return <div className="cell-list-item">{child}</div>;
+};
 
 export default CellListItem;
