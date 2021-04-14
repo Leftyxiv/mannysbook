dc9f1070832b1ef296d90c5044ee123967d81ae1
Leftyxiv <manny@aledoux.net>
2021-04-14 12:40:24 -0400

    added a show function

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -18,32 +18,38 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const cumulativeCode = useTypedSelector((state) => {
     const { data, order } = state.cells;
     const orderedCells = order.map((id) => data[id]);
-    const cumulativeCode = []
-    for(let c of orderedCells){
-      if (c.type === 'code'){
+    const cumulativeCode = [
+      `
+       const show = (value) => {
+         document.getElementById('root').innerHTML = value;
+       }
+      `
+    ];
+    for (let c of orderedCells) {
+      if (c.type === "code") {
         cumulativeCode.push(c.content);
       }
-      if (c.id === cell.id){
+      if (c.id === cell.id) {
         break;
       }
     }
     return cumulativeCode;
-  })
-  
+  });
+
   useEffect(() => {
     if (!bundle) {
-      createBundle(cell.id, cell.content);
+      createBundle(cell.id, cumulativeCode.join("\n"));
       return;
     }
     const timer = setTimeout(async () => {
-      createBundle(cell.id, cell.content);
+      createBundle(cell.id, cumulativeCode.join("\n"));
     }, 1000);
 
     return () => {
       clearTimeout(timer);
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [cell.content, cell.id, createBundle]);
+  }, [cumulativeCode.join("\n"), cell.id, createBundle]);
 
   return (
     <Resizable direction="vertical">
